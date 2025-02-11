2a5049d399c6c721dc41169c9fdccf60d36aa12e
asmile1559 <luoxudong24@mails.ucas.ac.cn>
2025-02-11 21:10:25 +0800

    login and register js module finished

--- app/frontend/main.go
+++ app/frontend/main.go
@@ -10,6 +10,7 @@ import (
 
 	bizrouter "github.com/asmile1559/dyshop/app/frontend/biz/router"
 	rpcclient "github.com/asmile1559/dyshop/app/frontend/rpc"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
@@ -22,8 +23,9 @@ func main() {
 	rpcclient.InitRPCClient()
 
 	router := gin.Default()
-
+	router.Use(cors.Default())
 	router.LoadHTMLGlob("templates/**")
+
 	router.StaticFS("/static", http.Dir("static"))
 
 	router.GET("/ping", func(c *gin.Context) {
@@ -60,8 +62,39 @@ func registerTestRouter(e *gin.Engine) {
 	}
 
 	_test := e.Group("/test")
+	_test.GET("/register", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "register.html", gin.H{})
+	})
+	_test.POST("/register", func(c *gin.Context) {
+		u := struct {
+			Email           string `json:"email"`
+			Password        string `json:"password"`
+			ConfirmPassword string `json:"confirm_password"`
+		}{}
+		err := c.BindJSON(&u)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"code":    http.StatusBadRequest,
+				"message": "Expect json format register information",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"code":    http.StatusOK,
+			"message": "register ok!",
+			"token":   "111",
+		})
+	})
+
+	_test.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", gin.H{})
+	})
 	_test.POST("/login", func(c *gin.Context) {
-		u := user{}
+		u := struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}{}
 		err := c.BindJSON(&u)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
@@ -70,7 +103,7 @@ func registerTestRouter(e *gin.Engine) {
 			})
 			return
 		}
-		token, err := jwt.GenerateJWT(u.UserID)
+		token, err := jwt.GenerateJWT(uint32(1))
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"code":    http.StatusInternalServerError,
