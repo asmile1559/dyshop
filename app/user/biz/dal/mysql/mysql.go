a5674a12df64a05fe686ac4a504b65e44bdaca0b
jiny3 <jinyan1419@gmail.com>
2025-02-11 20:23:52 +0800

    🌈 style: 使用utils简化代码

--- app/user/biz/dal/mysql/mysql.go
+++ app/user/biz/dal/mysql/mysql.go
@@ -1,50 +1,32 @@
 package mysql
 
 import (
-	"fmt"
-
-	"github.com/asmile1559/dyshop/app/user/biz/model"
-	"github.com/spf13/viper"
-	"gorm.io/driver/mysql"
+	"github.com/asmile1559/dyshop/utils/db/mysqlx"
+	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
 // 定义全局变量 db 用于存储数据库连接实例
 var db *gorm.DB
 
-// initDB 初始化数据库连接
-func initDB() (err error) {
-	// 构建数据库连接的 DSN (数据源名称)
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		viper.GetString("database.username"),
-		viper.GetString("database.password"),
-		viper.GetString("database.host"),
-		viper.GetString("database.port"),
-		viper.GetString("database.dbname"),
-	)
-
-	// 使用 GORM 打开数据库连接
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+func Init(conf mysqlx.DbConfig) error {
+	var err error
+	db, err = mysqlx.New(conf)
 	if err != nil {
-		return err // 如果连接失败，返回错误
+		logrus.WithError(err).Fatal("数据库初始化失败")
+		return err
 	}
-
-	// 可选：自动迁移数据库，创建表（根据实际情况选择是否开启）
-	err = db.AutoMigrate(&model.User{})
-	if err != nil {
-		return fmt.Errorf("数据库迁移失败: %v", err)
-	}
-	
-	return
-}
-
-
-// 初始化数据库连接，供其他文件调用
-func Init() error {
-	return initDB()
+	return nil
 }
 
 func Close() {
-	sqlDB, _ := db.DB()
-	_ = sqlDB.Close()
+	sqlDB, err := db.DB()
+	if err != nil {
+		logrus.WithError(err).Error("获取数据库失败")
+		return
+	}
+	err = sqlDB.Close()
+	if err != nil {
+		logrus.WithError(err).Error("关闭数据库失败")
+	}
 }
--- app/user/main.go
+++ app/user/main.go
@@ -4,49 +4,40 @@ import (
 	"net"
 
 	"github.com/asmile1559/dyshop/app/user/biz/dal/mysql"
+	"github.com/asmile1559/dyshop/app/user/biz/model"
 	pbuser "github.com/asmile1559/dyshop/pb/backend/user"
-	"github.com/asmile1559/dyshop/utils/logx"
+	"github.com/asmile1559/dyshop/utils/db/mysqlx"
+	"github.com/asmile1559/dyshop/utils/hookx"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
 
+func init() {
+	hookx.Init(hookx.DefaultHook)
+}
+
 func main() {
-
-	if err := loadConfig(); err != nil {
-		logrus.Fatal(err)
+	dbconf := mysqlx.DbConfig{
+		User:     viper.GetString("database.username"),
+		Password: viper.GetString("database.password"),
+		Host:     viper.GetString("database.host"),
+		Port:     viper.GetInt("database.port"),
+		DbName:   viper.GetString("database.dbname"),
+		Models:   []any{model.User{}},
 	}
-
-	initLog()
-
-	// 初始化MYSQL
-	err := mysql.Init()
-	if err != nil {
-		logrus.Fatal("数据库初始化失败: ", err)
-	}
-	
+	mysql.Init(dbconf)
 	defer mysql.Close()
-	
+
 	cc, err := net.Listen("tcp", ":"+viper.GetString("server.port"))
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	
+
 	s := grpc.NewServer()
 
 	pbuser.RegisterUserServiceServer(s, &UserServiceServer{})
 	if err = s.Serve(cc); err != nil {
 		logrus.Fatal(err)
 	}
-}
-
-func loadConfig() error {
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("conf")
-	return viper.ReadInConfig()
-}
-
-func initLog() {
-	logx.Init(logrus.InfoLevel)
 }
