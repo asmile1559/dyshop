23be5463e061248dd6e5b49d2bb9cb645e6d8bac
jiny3 <jinyan1419@gmail.com>
2025-02-11 21:53:03 +0800

    ✨ feat: user add metrics

--- app/user/main.go
+++ app/user/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/asmile1559/dyshop/app/user/biz/dal/mysql"
 	"github.com/asmile1559/dyshop/app/user/biz/model"
 	pbuser "github.com/asmile1559/dyshop/pb/backend/user"
 	"github.com/asmile1559/dyshop/utils/db/mysqlx"
 	"github.com/asmile1559/dyshop/utils/hookx"
+	"github.com/asmile1559/dyshop/utils/mtl"
 	"github.com/asmile1559/dyshop/utils/registryx"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -41,6 +45,20 @@ func main() {
 	if len(services) == 0 {
 		logrus.Fatalf("No services found in config")
 	}
+
+	// 注册 Metrics
+	address := services[0].(map[string]any)["address"].(string)
+	host := strings.Split(address, ":")[0]
+	info := mtl.MetricsInfo{
+		Prefix: prefix,
+		Target: fmt.Sprintf("%s:2113", host),
+		Labels: map[string]string{
+			"type": "apps",
+			"app":  "user",
+		},
+	}
+	mtl.RegisterMetrics(info)
+	defer mtl.DeregisterMetrics(info)
 
 	// 启动服务实例并注册到 Etcd
 	registryx.StartEtcdServices(
