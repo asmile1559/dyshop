223a274020a9f4c0c1f46f1b91ae4475611cfcc9
fwbc <1541483198@qq.com>
2025-02-28 18:53:55 +0800

    table sql

--- app/user/biz/model/create_table.sql
+++ app/user/biz/model/create_table.sql
@@ -1,20 +1,14 @@
 CREATE TABLE `users` (
     `user_id` bigint NOT NULL,
     `email` varchar(191) NOT NULL,
-    `password` longtext NOT NULL,
+    `password` longtext,
+    `phone` longtext,
+    `name` longtext,
+    `sign` longtext,
+    `gender` longtext,
+    `birthday` datetime(3) DEFAULT NULL,
+    `role` longtext,
+    `url` longtext,
     PRIMARY KEY (`user_id`),
     UNIQUE KEY `uni_users_email` (`email`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3
-
-CREATE TABLE `users` (
-    `user_id` BIGINT NOT NULL PRIMARY KEY,    -- 用户ID（雪花算法生成）
-    `email` VARCHAR(191) NOT NULL UNIQUE,     -- 邮箱（唯一约束）
-    `password` VARCHAR(255) NOT NULL,         -- 加密存储的密码
-    `phone` VARCHAR(20) DEFAULT NULL,         -- 手机号（可选）
-    `name` VARCHAR(100) DEFAULT NULL,         -- 用户昵称
-    `sign` VARCHAR(255) DEFAULT NULL,         -- 个性签名
-    `gender` ENUM('男', '女', '不愿意公开') DEFAULT '不愿意公开', -- 性别（男、女、不愿意公开）
-    `birthday` DATE DEFAULT NULL,             -- 生日
-    `role` ENUM('user', 'merchant') DEFAULT 'user', -- 角色（普通用户、商户）
-    `url` VARCHAR(255) DEFAULT NULL   -- 头像存储URL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
