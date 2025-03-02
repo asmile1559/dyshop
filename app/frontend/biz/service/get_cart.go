fe7ed222e50afac3a1a9383e3430823173213bfa
qiannian0116 <qiannian116@gamil.com>
2025-03-02 15:40:38 +0000

    ✨ feat: 获取产品和用户信息并返回前端

--- app/frontend/biz/service/get_cart.go
+++ app/frontend/biz/service/get_cart.go
@@ -6,6 +6,8 @@ import (
 
 	rpcclient "github.com/asmile1559/dyshop/app/frontend/rpc"
 	pbcart "github.com/asmile1559/dyshop/pb/backend/cart"
+	pbproduct "github.com/asmile1559/dyshop/pb/backend/product"
+	pbuser "github.com/asmile1559/dyshop/pb/backend/user"
 	"github.com/asmile1559/dyshop/pb/frontend/cart_page"
 	"github.com/gin-gonic/gin"
 )
@@ -18,26 +20,65 @@ func NewGetCartService(c context.Context) *GetCartService {
 	return &GetCartService{ctx: c}
 }
 
-func (s *GetCartService) Run(_ *cart_page.GetCartReq) (map[string]interface{}, error) {
+func (s *GetCartService) Run(_ *cart_page.GetCartReq) ([]gin.H, gin.H, error) {
 	id, ok := s.ctx.Value("user_id").(int64)
 	if !ok {
-		return nil, errors.New("no user id in context")
+		return nil, nil, errors.New("no user id in context")
 	}
 
 	cartClient, conn, err := rpcclient.GetCartClient()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	defer conn.Close()
-
 	resp, err := cartClient.GetCart(s.ctx, &pbcart.GetCartReq{
 		UserId: uint32(id),
 	})
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return gin.H{
-		"resp": resp,
-	}, nil
+	productClient, conn, err := rpcclient.GetProductClient()
+	if err != nil {
+		return nil, nil, err
+	}
+	defer conn.Close()
+	respList := []gin.H{}
+	for _, item := range resp.Items {
+		productInfo, err := productClient.GetProduct(s.ctx, &pbproduct.GetProductReq{
+			Id: item.ProductId,
+		})
+		if err != nil {
+			return nil, nil, err
+		}
+		itemMap := gin.H{
+			"ItemId":      item.Id,
+			"ProductId":   item.ProductId,
+			"ProductImg":  productInfo.Product.Picture,
+			"ProductName": productInfo.Product.Name,
+			"ProductSpec": gin.H{
+				"Name":  "",
+				"Price": productInfo.Product.Price,
+			},
+			"Quantity": item.Quantity,
+			"Postage":  "10",
+		}
+		respList = append(respList, itemMap)
+	}
+	userClient, conn, err := rpcclient.GetUserClient()
+	if err != nil {
+		return nil, nil, err
+	}
+	defer conn.Close()
+	userInfo, err := userClient.GetUserInfo(s.ctx, &pbuser.GetUserInfoReq{
+		UserId: id,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	userResp := gin.H{
+		"Name": userInfo.Name,
+	}
+
+	return respList, userResp, nil
 }
