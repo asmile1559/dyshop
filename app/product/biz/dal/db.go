cfa130efcb44c64aed87ff71d3dc638b5519421e
jiny3 <jinyan1419@gmail.com>
2025-03-02 14:23:27 +0800

    🦄 refactor: simplify code by utils

--- app/product/biz/dal/db.go
+++ app/product/biz/dal/db.go
@@ -1,37 +1,29 @@
 package dal
 
 import (
-	"fmt"
-
 	"github.com/asmile1559/dyshop/app/product/biz/model"
-	"gorm.io/driver/mysql"
+	"github.com/asmile1559/dyshop/utils/db/mysqlx"
+	"github.com/spf13/viper"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 // 全局 DB 连接
 var DB *gorm.DB
 
-// InitDB 根据传入的 dsn 初始化 gorm.DB
-func InitDB(dsn string) error {
-	dsn = "user:123456@tcp(127.0.0.1:3308)/dyshop?charset=utf8mb4&parseTime=True&loc=Local"
-	newDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+func InitDB() error {
+	conf := mysqlx.DbConfig{
+		User:     viper.GetString("database.username"),
+		Password: viper.GetString("database.password"),
+		Host:     viper.GetString("database.host"),
+		Port:     viper.GetInt("database.port"),
+		DbName:   viper.GetString("database.dbname"),
+		Models:   []any{&model.Product{}},
+	}
+
+	db, err := mysqlx.New(conf)
 	if err != nil {
-		return fmt.Errorf("failed to open mysql: %w", err)
+		return err
 	}
-
-	DB = newDB
-
-	// 让 GORM 自动建表或迁移
-	if err = autoMigrate(); err != nil {
-		return fmt.Errorf("failed to auto-migrate: %w", err)
-	}
+	DB = db
 	return nil
-}
-
-// autoMigrate 执行自动迁移
-func autoMigrate() error {
-	return DB.AutoMigrate(&model.Product{})
 }
